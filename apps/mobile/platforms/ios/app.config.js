1debe9fe02565e434f3102c78b5fa470d47a2774
Bhargav Kodali <kodalibhargav019@gmail.com>
2026-02-25 06:25:37 -0700

    fix: allow expo to dicover workspace packages

---
+++ apps/mobile/platforms/ios/app.config.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import("expo/config").ExpoConfig["ios"]}
+ */
+const iosConfig = {
+      "supportsTablet": true,
+      "bundleIdentifier": "com.anonymous.mobile"
+}
+
+export default iosConfig;
