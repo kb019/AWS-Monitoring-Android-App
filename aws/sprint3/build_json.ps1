04ae1204f6c92c20f08b3d50c1f1cc8544f27dee
Shrinidhi Mane <smane13@asu.edu>
2026-03-06 11:55:10 -0700

    Sprint 3: Integrated memory metric into structured output

--- aws/sprint3/build_json.ps1
+++ aws/sprint3/build_json.ps1
@@ -52,7 +52,8 @@ foreach($id in $IDS){
   $inst = aws ec2 describe-instances --instance-ids $id --region $REGION --output json | ConvertFrom-Json
   $name = ($inst.Reservations[0].Instances[0].Tags | Where-Object { $_.Key -eq "Name" } | Select-Object -First 1).Value
   $publicIp = $inst.Reservations[0].Instances[0].PublicIpAddress
-
+  $imageId = $inst.Reservations[0].Instances[0].ImageId
+  $instanceType = $inst.Reservations[0].Instances[0].InstanceType
   $cpu = aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name CPUUtilization `
     --dimensions Name=InstanceId,Value=$id --start-time $START_STR --end-time $END_STR --period $PERIOD --statistics Average --region $REGION --output json | ConvertFrom-Json
 
@@ -67,7 +68,24 @@ foreach($id in $IDS){
 
   $dwrite = aws cloudwatch get-metric-statistics --namespace AWS/EC2 --metric-name DiskWriteBytes `
     --dimensions Name=InstanceId,Value=$id --start-time $START_STR --end-time $END_STR --period $PERIOD --statistics Sum --region $REGION --output json | ConvertFrom-Json
+  # ----- MEMORY (CWAgent) -----
+  # ensure $imageId and $instanceType exist (you added them earlier)
+  $mem = aws cloudwatch get-metric-statistics --namespace CWAgent --metric-name mem_used_percent `
+    --dimensions Name=InstanceId,Value=$id Name=ImageId,Value=$imageId Name=InstanceType,Value=$instanceType `
+    --start-time $START_STR --end-time $END_STR --period $PERIOD --statistics Average --region $REGION --output json | ConvertFrom-Json
 
+  $memoryLatest = if($mem.Datapoints.Count -gt 0){ ($mem.Datapoints | Sort-Object Timestamp)[-1].Average } else { $null }
+    $memoryDatapoints = @()
+  if ($mem.Datapoints.Count -gt 0) {
+    $memoryDatapoints = $mem.Datapoints |
+      Sort-Object Timestamp |
+      ForEach-Object {
+        [PSCustomObject]@{
+        timestamp = ([datetime]$_.Timestamp).ToUniversalTime().ToString("yyyy-MM-ddTHH:mm:ssZ")
+          value     = [math]::Round([double]$_.Average, 2)
+        }
+      }
+  }
   $r = [PSCustomObject]@{
     instanceId      = $id
     name            = $name
@@ -78,7 +96,8 @@ foreach($id in $IDS){
     netoutLatest    = if($netout.Datapoints.Count -gt 0){($netout.Datapoints| Sort-Object Timestamp)[-1].Sum     } else { $null }
     diskReadLatest  = if($dread.Datapoints.Count -gt 0){ ($dread.Datapoints | Sort-Object Timestamp)[-1].Sum     } else { $null }
     diskWriteLatest = if($dwrite.Datapoints.Count -gt 0){($dwrite.Datapoints| Sort-Object Timestamp)[-1].Sum     } else { $null }
-
+   memoryLatest   = $memoryLatest
+       memoryDatapoints = $memoryDatapoints
     startTimeUtc    = $START_STR
     endTimeUtc      = $END_STR
     periodSeconds   = $PERIOD
@@ -87,6 +106,6 @@ foreach($id in $IDS){
   $result += $r
 }
 
-$outFile = "aws/sprint2/instances_metrics.json"
+$outFile = "instances_metrics.json"
 $result | ConvertTo-Json -Depth 6 | Out-File $outFile -Encoding utf8
 Write-Output "Saved $outFile"
